7a836a7149109d27a9cfd13604fd580078686b6e
oleh_yudin <olegyudin15@gmail.com>
2025-04-23 13:34:00 +0300

    add check-console

--- js/task-3.js
+++ js/task-3.js
@@ -13,3 +13,11 @@ const profile = {
     }
 };
 
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
